d0f10647cdda2a5aec78434fd426c4724bf6b833
German Bariguian <germanbariguian@gmail.com>
2022-10-22 01:54:16 -0300

    Proyecto Bariguian

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
 
 router.get('/', async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 60,
+        height: 60,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,  // titulo y cuerpo
+        imagen       // imagen
+      }
+    } else {
+      return {
+        ...novedad, //titulo y cuerpo
+        imagen: ''  // nada
+      }
+    }
+  });
 
   res.render('admin/novedades', {
     layout: 'admin/layout',
@@ -25,6 +47,69 @@ router.get('/agregar', (req, res, next) => {
   res.render('admin/agregar', {  // agregar.hbs
     layout: 'admin/layout',
   });
+});
+
+router.post('/agregar', async (req, res, next) => {
+  try {
+
+    var img_id = '';
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
+    if (req.body.titulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.insertNovedad({
+        ...req.body,  // spread titulo y cuerpo
+        img_id
+      });
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargó la novedad'
+    })
+  }
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+})
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      cuerpo: req.body.cuerpo
+    }
+
+    console.log(obj)
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+
+  } catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modificó la novedad'
+    })
+  }
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,32 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require("nodemailer");
 var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades()
+
+  novedades = novedades.splice(0, 5); // seleccionamos las primeras 5 novedades
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,  // titulo y cuerpo
+        imagen       // imagen
+      }
+    } else {
+      return {
+        ...novedad, //titulo y cuerpo
+        imagen: ''  // nada
+      }
+    }
+  });
 
   res.render('index', {
     novedades
